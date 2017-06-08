d86e8e7002611f1b679f9311e6e191f96166d758
Luke-mc <luke_mcglynn@yahoo.com>
2017-06-07 19:31:39 -1000

    RangedZombie

--- zombies.js
+++ zombies.js
@@ -444,6 +444,12 @@ class FastZombie extends Zombie {
  * @param {number} speed            The zombie's speed.
  */
 
+ class RangedZombie extends Zombie {
+  constructor(health,strength,speed){
+    super(health,strength,speed);
+  }
+}
+
 
 /**
  * RangedZombie Extends Zombie Class
