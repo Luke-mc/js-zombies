0ef8f5d69aa6626a54465b9aae4c982fd90d04c5
Luke-mc <luke_mcglynn@yahoo.com>
2017-06-07 19:23:48 -1000

    useItem

--- zombies.js
+++ zombies.js
@@ -310,6 +310,14 @@ discardItem(item){
  * @param {Item/Weapon/Food} item   The item to use.
  */
 
+ useItem(item){
+    if (item instanceof Weapon){
+    this.equip(item);
+  }else if (item instanceof Food){
+    this.eat(item);
+  }
+ }
+
 
 /**
  * Player Class Method => equippedWith()
